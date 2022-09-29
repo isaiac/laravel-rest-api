2320cfcbf5f535cb951e440907a79adea1cd7790
Isaí Ayala César <isaiacwebdev@gmail.com>
2022-09-29 16:59:02 -0500

    Change the default api configuration

--- config/api.php
+++ config/api.php
@@ -137,10 +137,10 @@ return [
     */
 
     'errorFormat' => [
-        'message' => ':message',
-        'errors' => ':errors',
         'code' => ':code',
         'status_code' => ':status_code',
+        'message' => ':message',
+        'errors' => ':errors',
         'debug' => ':debug',
     ],
 
