03c23d399011f774ce94df6c35f8f2c64546c3d0
Isaí Ayala César <isaiacwebdev@gmail.com>
2022-09-29 17:14:28 -0500

    Create all the basic resources

---
+++ app/Http/Resources/Log/LogResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Log;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LogResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'log_name' => $this->log_name,
+            'description' => $this->description,
+            'event' => $this->event,
+            'subject_type' => $this->subject_type,
+            'subject_id' => $this->subject_id,
+            'causer_type' => $this->causer_type,
+            'causer_id' => $this->causer_id,
+            'properties' => $this->properties,
+            'batch_uuid' => $this->batch_uuid,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
---
+++ app/Http/Resources/Me/MeResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Me;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'username' => $this->username,
+            'status' => $this->status,
+            'email_verified_at' => $this->email_verified_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+            'roles' => RoleResource::collection($this->roles),
+            'permissions' => PermissionResource::collection($this->permissions)
+        ];
+    }
+}
