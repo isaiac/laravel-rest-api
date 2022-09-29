be8b4e04030002261f4c19740de1bb2f77df8678
Isaí Ayala César <isaiacwebdev@gmail.com>
2022-09-29 17:20:16 -0500

    Add the default boot configuration for the api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,16 @@
 
 namespace App\Providers;
 
+use Dingo\Api\Exception\Handler as ExceptionHandler;
+use Dingo\Api\Http\Response\Factory as Response;
+use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Exceptions\MissingAbilityException;
+use Laravel\Sanctum\Exceptions\MissingScopeException;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Sanctum::ignoreMigrations();
+
+        $this->registerExceptionHandlers();
     }
 
     /**
@@ -23,6 +34,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (in_array(config('app.env'), ['production', 'testing'])) {
+            URL::forceScheme('https');
+        }
+
+        JsonResource::withoutWrapping();
+    }
+
+    /**
+     * Register all the application exception handlers.
+     *
+     * @return void
+     */
+    protected function registerExceptionHandlers()
+    {
+        app(ExceptionHandler::class)->register(function (Exception $e) {
+            if ($e instanceof AuthenticationException) {
+                return app(Response::class)->errorUnauthorized();
+            }
+
+            if ($e instanceof MissingAbilityException
+                || $e instanceof MissingScopeException
+            ) {
+                return app(Response::class)->errorForbidden($e->getMessage());
+            }
+        });
     }
 }
